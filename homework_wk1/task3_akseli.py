bf8f5892188bfa58770e120821ddec042fbd98c7
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-01-21 10:45:59 +0200

    #5 Updated code to fetch, parse and print as per issue.

--- homework_wk1/task3_akseli.py
+++ homework_wk1/task3_akseli.py
@@ -1,1 +1,13 @@
 # This is and empty python file for initial commit purposes
+#what happens if I add a line
+import requests
+from bs4 import BeautifulSoup
+
+#Fetch webcontent and parse to variable. print variable
+
+url = "https://www.ilmatieteenlaitos.fi/saa/espoo"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+print(soup)
