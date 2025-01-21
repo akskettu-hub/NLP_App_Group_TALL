166b9ff46525574e8fe61fef8e97d63dd482b352
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-01-21 13:43:32 +0200

    Updated code to fetch content from FMI website and find when wether was last updated. A simplistic solution to the task, but I managed to figure out the basics of scraping

--- homework_wk1/task3_akseli.py
+++ homework_wk1/task3_akseli.py
@@ -3,11 +3,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-#Fetch webcontent and parse to variable. print variable
+def fetch_content(url : str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    return soup
+
+#Fetches info on when the wether report was las updated. 
+def report_updated(soup):
+    header_panel = soup.find('div', class_="row local-weather-header-panel mx-0 mb-3 pt-3")
+    elements = header_panel.find_all('span')
+
+    rows = []
+    for element in elements:
+        text = element.text
+        rows.append(text)
+    
+    last_updated = rows[0] + rows[1]
+    
+    return last_updated
 
 url = "https://www.ilmatieteenlaitos.fi/saa/espoo"
+soup = fetch_content(url)
+data = report_updated(soup)
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-print(soup)
+print(data)
