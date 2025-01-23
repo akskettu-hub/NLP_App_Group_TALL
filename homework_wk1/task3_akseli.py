cd2376852dc60429987fa751d4946e921b05d3fe
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-01-23 10:02:49 +0200

    #6 Mysolution to task 3.

--- homework_wk1/task3_akseli.py
+++ homework_wk1/task3_akseli.py
@@ -1,5 +1,11 @@
+# Code for task 3, and therefore a little messy.
+# Prints when weather was last updated on the FMI website and latest articles published on cracked.com (in dataframe format)
+# Planned on extracting weather information but found that too tricky
+# Demonstrates basics of webscraping
+
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 def fetch_content(url : str):
     response = requests.get(url)
@@ -7,8 +13,8 @@ def fetch_content(url : str):
 
     return soup
 
-#Fetches info on when the wether report was las updated. 
-def report_updated(soup):
+#Fetches info on when the weather report was las updated. 
+def report_updated_FMI(soup):
     header_panel = soup.find('div', class_="row local-weather-header-panel mx-0 mb-3 pt-3")
     elements = header_panel.find_all('span')
 
@@ -21,28 +27,44 @@ def report_updated(soup):
     
     return last_updated
 
-#does not work
-def latest_observations(soup):
-    latest = soup.find_all('div', class_='my-1 w-25 col-12 col-md-6')
+#Fetches latest articles from cracked.com and puts each article and its data into a dictonary for easy pandas dataframe.
+def latest_cracked(soup):
+    latest = soup.find('div', class_="right")
+    articles = latest.find_all('article')
     
-    #out = []
-    #for element in latest:
-    #    out.append(element.text)
+    text_articles = []
+    for article in articles:
+        title = article.find('h2').text
+        date = article.find('time').text
+        topic = article.find('label').text
+        author = article.find('dd').a.text
+        link = article.find('h2').a['href']
     
-    return latest
+        text_articles.append({"Title" : title, "Date Published" : date, "Topic" : topic, "Author" : author, "Link" : link})
+        
+    return text_articles
+
+def df_from_dict(list_dicts : dict):
+    df = pd.DataFrame(list_dicts)
+    df['Author'] = df['Author'].str.replace(",", "")
+    return df
     
 
 if __name__ == "__main__":
     url = "https://www.ilmatieteenlaitos.fi/saa/espoo"
     soup = fetch_content(url)
-    data = report_updated(soup)
+    data = report_updated_FMI(soup)
 
     print(data)
     
     url = "https://en.ilmatieteenlaitos.fi/local-weather/espoo"
     soup = fetch_content(url)
-    data = report_updated(soup)
+    data = report_updated_FMI(soup)
 
-    print(data)    
+    print(data)
     
+    url = "https://www.cracked.com/"
+    cracked_soup = fetch_content(url)
+    cracked_df = df_from_dict(latest_cracked(cracked_soup))
     
+    print(cracked_df)
