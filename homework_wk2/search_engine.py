b52284d5d6ac0f27e1f269651b9839d09db734e1
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2025-01-24 15:23:08 +0200

    ask user to type query  [issue 11]

--- homework_wk2/search_engine.py
+++ homework_wk2/search_engine.py
@@ -1,4 +1,3 @@
-
 from sklearn.feature_extraction.text import CountVectorizer
 
 
@@ -46,6 +45,8 @@ print(cv.vocabulary_) # note the _ at the end
 #.vocabulary_ (with a trailing underscore) is a Python dictionary
 print("Row index of 'example':", cv.vocabulary_["example"])
 print("Row index of 'silly':", cv.vocabulary_["silly"])
+
+
 
 #simple searches
 t2i = cv.vocabulary_  # shorter notation: t2i = term-to-index
@@ -148,6 +149,11 @@ for i, doc_idx in enumerate(hits_list):
 ### HOMEWORK
 # 1. Ask the user to type the query. Make this run in a loop, so that the user can run as many queries they like. Also come up with some user input that tells the program to quit, such as an empty string or a specific keyword. Type your code here: 
 
+# a) Ask the user to type the query (issue #11): 
+
+input = input("Please type in one of the following words: and, better, example, great, here, is, long, nothing, see, silly, this, to: ")
+print(td_matrix[t2i[input]])
+
 # 2.Your search application should print the contents of the retrieved documents. (A vector consisting of ones and zeros won't do!) If there are too many matching documents, maybe you want to show only the top n documents. (Still you could print out how many matching documents there are in total.) If the documents are long, maybe you want to truncate the output to the m first words or characters only. Type your code here: 
 
 # 3. If you just copy the code from the tutorial, your program will crash if you enter a word (term) that does not occur in any document in the collection. Modify your program to work correctly also in the case that a term is unknown. Type your code here: 
@@ -157,4 +163,3 @@ for i, doc_idx in enumerate(hits_list):
 # 5. This task is important. You need to index some "real" documents from a text file. When you run your program, it should start by reading document contents from a file and index these documents. After this, the user should be able to type queries and retrieve matching documents. Initially, you can use our example data sets: One contains 100 articles extracted from English Wikipedia and the other contains 1000 articles extracted from English Wikipedia (with topics mostly starting with the letter A). When you read these files, you need to produce a list of strings, such that an entire article (document) is in one string. You can locate the boundaries between two articles from the </article> tag, which always occurs on a line of its own in the file. The text is UTF-8 encoded. Type your code here: 
 
 # 6. If you like, you can use some other data, for instance a Wikipedia dump for some other language, such as Finnish: https://linguatools.org/tools/corpora/wikipedia-monolingual-corpora/. First you need to download a Wikipedia XML file. Then you need to uncompress it with bunzip2. Then you need to convert the XML format to plain text using the Perl script xml2txt.pl, which is available for download on the web page. You need to use the option -articles in order to preserve the article tags: perl xml2txt.pl -articles INPUT_FILE.xml OUTPUT_FILE.txt. Type your code here: 
-
