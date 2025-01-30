8d9aa41b92f807af54c7a75446baed583781464b
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-01-30 14:05:38 +0200

    \#19 Reformatted code quite extensively, almost all features of code are now their own functions. Some frankensteining might have happened between different contributor's code, as I organised code to be more easily understandable and coherent (at least that was the goal). Programme now works by simply running it.

--- homework_wk2/search_engine_clean.py
+++ homework_wk2/search_engine_clean.py
@@ -2,11 +2,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 import re
 
-
 documents = ["This is a silly example",
              "A better example",
              "Nothing to see here",
              "This is a great and long example"]
+
+# 4 The most likely reason why not all words are indexed is the default token pattern used by CountVectorizer: r'\b\w\w+\b' This pattern only matches words with two or more alphanumeric characters. See changed token_pattern when initalizing CountVectorizer at the top of the page
 
 cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r'\b\w+\b') ### changed token_pattern as part of homework #4
 sparse_matrix = cv.fit_transform(documents)
@@ -24,69 +25,76 @@ d = {"and": "&", "AND": "&",
      "not": "1 -", "NOT": "1 -",
      "(": "(", ")": ")"}          # operator replacements
 
-def rewrite_token(t):
-    return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) # Can you figure out what happens here?
-
-def rewrite_query(query): # rewrite every token in the query
-    return " ".join(rewrite_token(t) for t in query.split())
-
-#HOMEWORK
-
-# 1 - 3 
-def loop():
-    while True:
-        user_query = input("Please Enter your query, type 'quit' to exit: ")
-        if user_query == "quit":
-           print("Exit")
-           break
-        hits_matrix = eval(rewrite_query(user_query))
-       
-        print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
-        print("The coordinates of the non-zero elements:", hits_matrix.nonzero())  
-       
-        hits_list = list(hits_matrix.nonzero()[1])
-        if not hits_list:  
-           print("No matching document")
-        else:
-           for doc_idx in hits_list:
-               print("Matching doc:", documents[int(doc_idx)]) 
-
-        print(f"Found {len(hits_list)} matches:")
-    
-        print_limit = 2
-    
-        if len(hits_list) > print_limit:
-            print(f"Here are the first {print_limit} results:")
-        
-            e_list = list(enumerate(hits_list))
-            for i in range(print_limit):
-                print("Matching doc #{:d}: {:s}".format(e_list[i][0], documents[e_list[i][1]]))
-                
-        else:        
-            for i, doc_idx in enumerate(hits_list):
-                print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
-
-        
-loop()
-
-# 4 The most likely reason why not all words are indexed is the default token pattern used by CountVectorizer: r'\b\w\w+\b' This pattern only matches words with two or more alphanumeric characters. See changed token_pattern when initalizing CountVectorizer at the top of the page
-
 # 5
-
 def extract_articles(file):
     with open(file, 'r', encoding='utf-8') as f:
         text = f.read()
         
     articles = text.split("</article>")
-    cleaned = []
+    cleaned_articles = []
     for article in articles:
         
-        cleaned.append(re.sub(r"<.*>", "", article))
+        cleaned_articles.append(re.sub(r"<.*>", "", article))
         
-    return cleaned
+    return cleaned_articles
 
 small = "wiki_files/enwiki-20181001-corpus.100-articles.txt"
 large = "wiki_files/enwiki-20181001-corpus.1000-articles.txt"
 
+def user_query():
+    user_input = input("Please Enter your query, type 'quit' to exit: ")
+    return user_input
 
+#Modification of former rewrite_token() from course material that handles words not in documents
+def avoid_operators(t):
+   if t in d:
+       return d[t]
+   if t not in t2i:
+       return 'np.zeros((1, len(documents)), dtype=int)'
+   
+   return f'td_matrix[{t2i[t]}]'
 
+def rewrite_query(query):
+   return " ".join(avoid_operators(t) for t in query.split())
+
+def input_checker(user_input):
+    if user_input == "quit" or user_input == "":
+           print("Exit")
+           return False
+                
+def retrieve_matches(query):
+    hits_matrix = eval(rewrite_query(query))
+    hits_list = list(hits_matrix.nonzero()[1])
+    return hits_list
+        
+def print_retrieved(hits_list):
+    if not hits_list:  
+           print("No matching document")
+           
+    else:
+        print(f"Found {len(hits_list)} matches:")
+        
+        print_limit = 2
+        
+        if len(hits_list) > print_limit:
+            print(f"Here are the first {print_limit} results:")
+            
+            e_list = list(enumerate(hits_list))
+            for i in range(print_limit):
+                print("Matching doc #{:d}: {:s}".format(e_list[i][0], documents[e_list[i][1]]))
+                    
+        else:        
+            for i, doc_idx in enumerate(hits_list):
+                print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
+            
+def main():
+    while True:
+        user_input = user_query()
+        if input_checker(user_input) == False:
+            break
+        hits_list = retrieve_matches(user_input)
+        print_retrieved(hits_list)
+                
+
+if __name__ == "__main__":
+    main()
